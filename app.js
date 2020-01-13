a19b6e48ec634308496c5b5dd22bebabab61d1b1
Shubham Singh <Shubham11.singh@paytm.com>
2020-01-13 23:07:42 +0530

    Body Parse and Cors Error Handling (adding body-parser pkg)

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
 const morgan = require ('morgan');
+const bodyParser = require('body-parser');
 
-app.use(morgan('dev'));
 
 const productsRoutes = require('./api/router/products');
 const ordersRoutes = require('./api/router/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Request-With, Content-Type, Accepted, Authorization'
+        );
+    if(req.method === "OPTIONS") {
+        req.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
