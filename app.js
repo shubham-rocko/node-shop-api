c55f84ade2370c13dabf342760ca059819f59408
Shubham Singh <Shubham11.singh@paytm.com>
2020-01-15 18:35:12 +0530

    upload file using multer (form-data parser) and make upload folder public using express.static()

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const mongoose = require('mongoose');
 const productsRoutes = require('./api/router/products');
 const ordersRoutes = require('./api/router/orders');
 
+mongoose.promise = global.promise;
+
 mongoose.connect('mongodb+srv://node-shop-api:Shubham%406@cluster0-yr5nx.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }); //mongoAtlas connection
 
 app.use(morgan('dev')); //log to terminal whatever happen
+app.use('/uploads', express.static('uploads')); //make the folder public which is for everyone (externally used)
 app.use(bodyParser.urlencoded({extended: false})); //parse json body of the request
 app.use(bodyParser.json());
 
