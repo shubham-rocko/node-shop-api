03ffcf47c4df81cf80789e287af4b85968dcca21
Shubham Singh <Shubham11.singh@paytm.com>
2020-01-16 19:02:55 +0530

    add user login route and use jsonwebtoken pkg to create JWT from combimation of email and id and add expiration 1h time

--- api/router/users.js
+++ api/router/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -35,6 +36,56 @@ route.post('/signup', (req, res, next) => {
                 }
             });
         }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    User.find({email: req.body.email}).exec()
+    .then((user) => {
+        if(user.length < 1){
+            res.status(401).json({
+                message: "Auth failed"
+            });
+        } else {
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err){
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                } else if(result){
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, process.env.JWT_KEY, {
+                        expiresIn: "1h"
+                    });
+                    return res.status(200).json({
+                        message: "Auth Successful",
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: "Auth failed"
+                });
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: err});
+    });
+});
+
+route.delete("/:userId", (req, res, next) => {
+    User.deleteOne({_id: req.param.userId}).exec()
+    .then((result) => {
+        res.status(200).json({
+            message: "User removed"
+        });
+    })
+    .catch((err) => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
